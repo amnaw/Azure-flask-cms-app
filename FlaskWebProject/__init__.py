1d8ab5aa14c1b81a42165ea2ab97d3f2f6580ffd
amnaw <48104560+amnaw@users.noreply.github.com>
2022-10-24 00:02:14 +0300

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -7,34 +7,17 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
-from logging.config import dictConfig
-from flask.logging import default_handler
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# Add any logging levels and handlers with app.logger
-# logging.basicConfig(
-#    level=logging.WARNING,
-#    format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
-
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFOs',
-        'handlers': ['wsgi']
-    }
-})
-
-app.logger.removeHandler(default_handler)
+#-------------------------------------
+app.logger.setLevel(logging.WARNING)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.WARNING)
+app.logger.addHandler(streamHandler)
+#-------------------------------------
 
 Session(app)
 db = SQLAlchemy(app)
